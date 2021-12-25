9590070e6002ab31c878d7a39cf2312f990780d0
AcaciaSin <553777593@qq.com>
2021-12-25 15:47:04 +0800

    modify ARMusic

---
+++ ARMusic/ARMusic/InstrumentComponent.swift
@@ -0,0 +1,13 @@
+//
+//  InstrumentComponent.swift
+//  ARMusic
+//
+//  Created by 11 11 on 2021/12/25.
+//
+
+import RealityKit
+
+struct InstrumentComponent: Component {
+    var audioFile: String
+}
+
